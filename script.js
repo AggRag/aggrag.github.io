cc4fc556ca0bfc883c26894658d19bfc01fcf180
Raghav Aggarwal <aggarwal.raghav96@gmail.com>
2023-03-31 23:24:29 +0530

    Update code for fields

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
-function generatePDF() {
-	// Get form data
-	const name = document.getElementById("name").value;
-	const email = document.getElementById("email").value;
-	const phone = document.getElementById("phone").value;
-	const message = document.getElementById("message").value;
 
-	// Create PDF document
-	const doc = new jsPDF();
+// Function to handle form submit event
+function handleSubmit(event) {
+  event.preventDefault();
 
-	// Add content
-	doc.setFontSize(20);
-	doc.text("Form Data", 10, 20);
+  // Get the form data
+  var data = {
+    "Serial number": document.getElementById("serial_number").value,
+    "Date": document.getElementById("date").value,
+    "Name": document.getElementById("name").value,
+    "Age": document.getElementById("age").value,
+    "Sex": document.getElementById("sex").value,
+    "Address": document.getElementById("address").value,
+    "Diagnosis": document.getElementById("diagnosis").value,
+    "Procedures": document.getElementById("procedures").value,
+    "Operative Findings": document.getElementById("operative_findings").value,
+    "Outcome": document.getElementById("outcome").value,
+    "Identification": document.getElementById("identification").value,
+    "Contact": document.getElementById("contact").value,
+    "Hospital Name": document.getElementById("hospital_name").value,
+  };
 
-	doc.setFontSize(14);
-	doc.text(`Name: ${name}`, 10, 30);
-	doc.text(`Email: ${email}`, 10, 40);
-	doc.text(`Phone: ${phone}`, 10, 50);
-	doc.text(`Message: ${message}`, 10, 60);
 
-	// Save PDF
-	doc.save("form.pdf");
+  var csv = convertToCSV([data]);
+  downloadCSV(csv, 'medical_data.csv');
 }
+
+// Function to download CSV file
+function downloadCSV(csv, filename) {
+  var csvFile;
+  var downloadLink;
+
+  csvFile = new Blob([csv], {type: "text/csv"});
+  downloadLink = document.createElement("a");
+  downloadLink.download = filename;
+  downloadLink.href = window.URL.createObjectURL(csvFile);
+  downloadLink.style.display = "none";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+}
+
+// Function to convert data to CSV format
+function convertToCSV(objArray) {
+  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+  var str = '';
+  var header = "Serial number,Date,Name,Age,Sex,Address,Diagnosis,Procedures,Operative Findings,Outcome,Identification,Contact,Hospital Name\n";
+
+  for (var i = 0; i < array.length; i++) {
+    var line = '';
+    for (var index in array[i]) {
+      if (line != '') line += ','
+
+      line += array[i][index];
+    }
+
+    str += line + '\r\n';
+  }
+
+  return header + str;
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
